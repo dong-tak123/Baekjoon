b6e7ece7844b0d99f80e5ee5bb3bf63d28ba0afb
dong-tak123 <77623014+dong-tak123@users.noreply.github.com>
2022-02-13 15:01:03 +0900

    Add files via upload

--- Baekjoon/DFS, BFS/백준_19952_(인성_문제_있어__).py
+++ Baekjoon/DFS, BFS/백준_19952_(인성_문제_있어__).py
@@ -8,47 +8,40 @@ Original file is located at
 """
 
 from collections import deque
-def bfs(x, y, f):
+def bfs(x, y, GX, GY, f):
     q = deque()
-    q.append([x,y])
+    q.append([x,y,f])       #현재 남은 힘도 저장함
     visited[x][y] = True
     while q:
-        tx, ty = q.popleft()
-        if tx == gx and ty == gy:
+        tx, ty, tf = q.popleft()
+        if tx == GX and ty == GY:
             print("잘했어!!")
             return
         for i in range(4):
             nx = tx + dx[i]
             ny = ty + dy[i]
+            # 남은 힘이 0이하이면 더이상 이동 불가
+            if tf == 0:
+                continue
+            
+            # 움직일 수 있으면
             if 0<=nx<row and 0<=ny<col and not visited[nx][ny]:
-                if (graph[nx][ny]-graph[tx][ty]) <= f:
-                    q.append([nx, ny])
+                if (graph[nx][ny]-graph[tx][ty]) <= tf:
+                    q.append([nx, ny, tf-1])
                     visited[nx][ny] = True
-                else:
-                    f -= (graph[nx][ny] - graph[tx][ty])
-                    q.append([nx, ny])
-                    visited[nx][ny] = True
-    
     print("인성 문제있어??")
-                    
 
 t = int(input())
 for _ in range(t):
-    col, row, o, f, sx, sy, gx, gy = map(int, input().split())
+    row, col, o, f, sx, sy, gx, gy = map(int, input().split())
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
 
     graph = [[0] * col for _ in range(row)]
-    visited = [[False]*col for _ in range(row)]
+    visited = [[False] * col for _ in range(row)]
 
     for _ in range(o):
         X, Y, n = map(int, input().split())
         graph[X-1][Y-1] = n
 
-    bfs(sx-1, sy-1, f)
-
-print(f)
-
-for rows in graph:
-    print(*rows)
-
+    bfs(sx-1, sy-1, gx-1, gy-1, f)
